14bac927df5eeb372180317da71a880fb463c5b7
Gokul Gunasekaran <gokul@cask.co>
2017-06-20 18:04:36 -0700

    Adding FTP Put Action and unit test

---
+++ src/main/java/co/cask/hydrator/action/ftp/FTPActionConfig.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.hydrator.action.ftp;
+
+import co.cask.cdap.api.annotation.Description;
+import co.cask.cdap.api.annotation.Macro;
+import co.cask.cdap.api.plugin.PluginConfig;
+
+import javax.annotation.Nullable;
+
+/**
+ * Common config properties for FTP Action Plugins.
+ */
+public class FTPActionConfig extends PluginConfig {
+  @Description("Host name of the FTP server.")
+  @Macro
+  public String host;
+
+  @Description("Port on which FTP server is running. Defaults to 21.")
+  @Nullable
+  @Macro
+  public String port;
+
+  @Description("Name of the user used to login to FTP server. Defaults to 'anonymous'.")
+  @Nullable
+  @Macro
+  public String userName;
+
+  @Description("Password used to login to FTP server. Defaults to empty.")
+  @Nullable
+  @Macro
+  public String password;
+
+  @Description("Destination directory to which the files to be copied. If the directory does not exist," +
+    " it will be created.")
+  @Macro
+  public String destDirectory;
+
+  @Description("Regex to copy only the file names that match. By default, all files will be copied.")
+  @Nullable
+  public String fileNameRegex;
+
+  public FTPActionConfig(String host, String port, String userName, String password, String destDirectory,
+                         String fileNameRegex) {
+    this.host = host;
+    this.port = port;
+    this.userName = userName;
+    this.password = password;
+    this.destDirectory = destDirectory;
+    this.fileNameRegex = fileNameRegex;
+  }
+
+  public String getHost() {
+    return host;
+  }
+
+  public int getPort() {
+    return (port != null) ? Integer.parseInt(port) : 21;
+  }
+
+  public String getUserName() {
+    return (userName != null) ? userName : "anonymous";
+  }
+
+  public String getPassword() {
+    return (password != null) ? password : "";
+  }
+
+  public String getDestDirectory() {
+    return destDirectory;
+  }
+
+  public String getFileNameRegex() {
+    return (fileNameRegex != null) ? fileNameRegex : ".*";
+  }
+}
---
+++ src/main/java/co/cask/hydrator/action/ftp/FTPPutAction.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.hydrator.action.ftp;
+
+import co.cask.cdap.api.annotation.Description;
+import co.cask.cdap.api.annotation.Macro;
+import co.cask.cdap.api.annotation.Name;
+import co.cask.cdap.api.annotation.Plugin;
+import co.cask.cdap.etl.api.action.Action;
+import co.cask.cdap.etl.api.action.ActionContext;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+
+/**
+ * An {@link Action} that will copy files from File System to FTP Server.
+ */
+@Plugin(type = Action.PLUGIN_TYPE)
+@Name("FTPPut")
+public class FTPPutAction extends Action {
+  private static final Logger LOG = LoggerFactory.getLogger(FTPPutAction.class);
+  private FTPPutActionConfig config;
+
+  public FTPPutAction(FTPPutActionConfig config) {
+    this.config = config;
+  }
+
+  /**
+   * Configurations for the FTP Put Action Plugin.
+   */
+  public static class FTPPutActionConfig extends FTPActionConfig {
+
+    @Description("Directory or File on the Filesystem which needs to be copied to the FTP Server.")
+    @Macro
+    public String srcPath;
+
+    public FTPPutActionConfig(String host, String port, String userName, String password,
+                              String srcPath, String destDirectory, String fileNameRegex) {
+      super(host, port, userName, password, destDirectory, fileNameRegex);
+      this.srcPath = srcPath;
+    }
+
+    public String getSrcPath() {
+      return srcPath;
+    }
+  }
+
+  @Override
+  public void run(ActionContext context) throws Exception {
+    Path source = new Path(config.getSrcPath());
+    FileSystem fileSystem = FileSystem.get(new Configuration());
+    if (!fileSystem.exists(source)) {
+      throw new RuntimeException(String.format("Source Files don't exist at %s", source));
+    }
+
+    FTPClient ftp = null;
+    try {
+      ftp = FTPUtils.getFTPClient(config.getHost(), config.getPort(), config.getUserName(), config.getPassword());
+
+      // No easy way to check existence of directory, create directory if not present
+      boolean dirExists = ftp.changeWorkingDirectory(config.getDestDirectory());
+      if (!dirExists) {
+        ftp.makeDirectory(config.getDestDirectory());
+        ftp.changeWorkingDirectory(config.getDestDirectory());
+      }
+
+      // Filter out only the files to copy
+      FileStatus[] filesToCopy = fileSystem.listStatus(source, new PathFilter() {
+        @Override
+        public boolean accept(Path path) {
+          String fileName = path.getName();
+          return fileName.matches(config.getFileNameRegex());
+        }
+      });
+
+      for (FileStatus file : filesToCopy) {
+        Path filePath = file.getPath();
+        try (InputStream inputStream = fileSystem.open(filePath)) {
+          boolean success = ftp.storeFile(filePath.getName(), inputStream);
+          if (!success) {
+            LOG.error("Error copying file : {}", filePath);
+          }
+        }
+      }
+
+      ftp.logout();
+    } finally {
+      if (ftp != null && ftp.isConnected()) {
+        try {
+          ftp.disconnect();
+        } catch (Throwable t) {
+          LOG.error("Failure to disconnect the ftp connection.", t);
+        }
+      }
+    }
+  }
+}
